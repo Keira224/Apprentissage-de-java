f3fcfce4211cbd1ce5852e0be5d4a6c2351b5e85
Keira <ousmanekeira224@gmail.com>
2025-11-14 19:49:14 +0000

    Ajout de la fonction de vérification des nombres dans la classe hello

--- hello.java
+++ hello.java
@@ -5,10 +5,26 @@ public class hello{
         //Appel de la fonction 
         int somme = addition(5, 10);
         System.out.println("La somme de 5 et 10 est : " + somme);
+
+        //Appel de la fonction de verification
+        verifiNombre(-3);
     }
     //Declaration de la fonction 
     public static int addition(final int a, final int b){
         return a + b;
     }
-    
+    // les instruction if et else
+    public static void verifiNombre(final int nombre)
+    {
+        if (nombre > 0){
+            System.out.println("le nombre est positif");
+
+        }
+        else if (nombre < 0){
+            System.out.println("le nombre est negatif");
+        } else {
+            System.out.println("le nombre est zero");
+        }
+    }
 }
+    
