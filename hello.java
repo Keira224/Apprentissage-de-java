56e83abca9f9da537d1957b23cab1449d349b1fe
Keira <ousmanekeira224@gmail.com>
2025-11-14 19:32:23 +0000

    Ajout de la fonction addition

--- hello.java
+++ hello.java
@@ -1,5 +1,14 @@
 public class hello{
     public static void main(String[] args){
         System.out.println("Hello les developpeurs java !");
+
+        //Appel de la fonction 
+        int somme = addition(5, 10);
+        System.out.println("La somme de 5 et 10 est : " + somme);
     }
+    //Declaration de la fonction 
+    public static int addition(final int a, final int b){
+        return a + b;
+    }
+    
 }
